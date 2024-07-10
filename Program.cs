d2758d02396159142e56977f76e114f3c94c653b
Kyselov Yehor <kiselev883@gmail.com>
2024-07-10 17:53:05 +0300

    * Use correct directory for exePath

--- Program.cs
+++ Program.cs
@@ -3,7 +3,8 @@ using NBU_Currency_Rate_Monitor;
 IHost host = Host.CreateDefaultBuilder(args)
     .ConfigureAppConfiguration((context, config) =>
     {
-        config.SetBasePath(Directory.GetCurrentDirectory());
+        var exePath = Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location);
+        config.SetBasePath(exePath);
         config.AddJsonFile("appsettings.json", optional: false, reloadOnChange: true);
     })
     .ConfigureServices((context, services) =>
